9f1f0920bfc302e7448b407c03580b40b0767dfb
cavej <ozangencay123@gmail.com>
2023-08-13 11:26:24 +0300

    update browser screenshot

--- screenshot.ts
+++ screenshot.ts
@@ -1,18 +1,34 @@
-import { mkdirSync } from "fs";
-import puppeteer from "puppeteer"
+import { mkdirSync, unlink, unlinkSync } from "fs";
+import puppeteer from "puppeteer";
+import sharp from "sharp";
 
 async function captureScreenshot(id: string, subreddit: string) {
-  const targetUrl = `https://www.reddit.com/r/${subreddit}/comments/${id}`
+  const targetUrl = `https://www.reddit.com/r/${subreddit}/comments/${id}`;
   try {
-    const browser = await puppeteer.launch({headless: true})
+    const browser = await puppeteer.launch({ headless: true });
     const page = await browser.newPage();
-    await page.setViewport({width:1440, height:1080})
-    await page.goto(targetUrl)
-    await page.screenshot({path: `Data/${id}/title.png`})
-    await browser.close()
+    await page.setViewport({ width: 1440, height: 1080 });
+    await page.goto(targetUrl);
+    await page.screenshot().then((buffer) => {
+      try {
+
+        cropScreenshot(buffer, `Data/${id}/title.png`);
+      } catch(err) {
+        console.error(err)
+      }
+    });
+    await browser.close();
   } catch (err) {
-    throw err
+    throw err;
   }
 }
 
-export default captureScreenshot
+function cropScreenshot(path: Buffer, output: string) {
+  sharp(path)
+    .extract({ left: 270, top: 50, width: 800, height: 600 })
+    .toFile(output, function (err) {
+      if (err) throw err;
+    });
+}
+
+export default captureScreenshot;
